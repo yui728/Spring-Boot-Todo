e57eeb6949122c155bb83be4af5773342173e729
yui728 <yui.nishimra.1984@gmail.com>
2021-03-31 19:28:09 +0900

    ・MySQLコンテナの起動設定を追加

--- docker/docker/db/sql/init-database.sh
+++ docker/docker/db/sql/init-database.sh
@@ -3,4 +3,4 @@
 #sleep 90s
 
 # run the setup script to create the DB and the schema in the DB
-mysql -u docker -pdocker test_database < "/docker-entrypoint-initdb.d/001-create-tables.sql"
+# mysql -u docker -pdocker test_database < "/docker-entrypoint-initdb.d/001-create-tables.sql"
